7ae225b97cc430dbf5b795748e9678d50de88c8f
wwmBlog <lmorris@qq.com>
2012-02-12 17:13:10 +0800

    Changed a bit the skill dat
    Signed-off-by: wwmBlog <lmorris@qq.com>

--- js/pathControl.js
+++ js/pathControl.js
@@ -10,6 +10,30 @@ define(function(require) {
         titleClass : "pathItemTitle",
         minWidth   : 52,
         duration   : 250,
+        getToolTip : function(d)
+        {
+            var tags = d.tags,
+                tt   = d.lv ? "<div class='star'><span class='star" + d.lv 
+                              + "'></span></div>" + d.detail : d.detail;
+
+            if (tags)
+            {
+                tt += "<ul>";
+                for (var i = 0, len = tags.length; i < len; i+=2)
+                {
+                    tt += "<li class='tag'><span class='tagMain'>" + tags[i] 
+                          + "</span><span class='tagSub";
+                    if (tags[i+1])
+                    {
+                        tt += "'>" + tags[i+1] + "</span></li>";
+                    } else {
+                        tt += " clean'></span></li>"
+                    }
+                }
+                tt += "</ul>";
+            }
+            return tt;
+        }
     };
 
     function PathControl(pathControlID, jsonData)
@@ -31,7 +55,7 @@ define(function(require) {
                         .appendTo(control.$div)
                         .data(d)
                         .hover(pcItemHover, pcItemBlur)
-                        .tooltip(d.detail);
+                        .tooltip(optns.getToolTip(d));
 
             if (!control.firstLowest) {
                 $ch.css("z-index", len - i);
@@ -44,115 +68,7 @@ define(function(require) {
         }
 
         // Resize and arrange.
-        $(window).resize(function(){ resizePathControl(); });
-        resizePathControl();
-
-        function pcItemBlur()
-        {
-            // Animate to weighted width
-            $div.children().each(
-                function()
-                {
-                    var t = $(this);
-                    t.animate({
-                        left  : t.data("weightedLeft"),
-                        width : t.data("weightedWidth")
-                    }, optns.duration);
-                }
-            );
-        }
-
-        function pcItemHover() 
-        {
-            // Animate to prefered width
-            var $this = $(this),
-                pw    = $this.data("preferWidth"),
-                need  = pw - $this.data("weightedWidth");
-            if (need <= 0) { return; }
-
-            var $sibling  = $this.next(),
-                gain      = 0,
-                rGains    = [],
-                rSiblings = [];
-
-            while ($sibling.size() != 0 && gain < need) 
-            {
-                rSiblings.push($sibling);
-
-                var canget = $sibling.data("weightedWidth") - optns.minWidth;
-                if (canget + gain > need) { canget = need - gain; }
-                gain += canget;
-
-                rGains.push(canget);
-                $sibling = $sibling.next();
-            }
-
-            var lneed = need - gain;
-
-            if(gain != 0) 
-            {
-                var rgain = gain;
-                for(i = 0; i < rSiblings.length; ++i)
-                {
-                    $sibling = rSiblings[i]
-                    $sibling.stop(true, false)
-                            .animate({
-                                left  : $sibling.data("weightedLeft")+rgain+"px",
-                                width : $sibling.data("weightedWidth")-rGains[i]+"px"
-                            }, optns.duration);
-                    rgain -= rGains[i];
-                }
-            }
-
-            var prevSize = $this.prevAll().size();
-            if(lneed > 0 && prevSize > 0)
-            {
-                var leftMinWidth = prevSize * optns.minWidth,
-                    leftMaxGain  = $this.data("weightedLeft") - leftMinWidth;
-
-                if (!control.firstLowest) {
-                    leftMaxGain += $this.outerWidth() - $this.width();
-                }
-
-                if (leftMaxGain <= 0) {
-                    lneed = 0;
-                } else if(lneed > leftMaxGain) {
-                    lneed = leftMaxGain;
-                }
-            }
-
-            var tleft  = $this.data("weightedLeft") - lneed;
-            if (tleft > 0 && (gain > 0 || lneed > 0))
-            {
-                $this.stop(true, false)
-                 .animate({
-                    left  : tleft + "px",
-                    width : pw + "px"
-                 }, optns.duration);
-            }
-
-            $sibling = $this.prev();
-            while(lneed > 0 && $sibling.size() != 0) 
-            {
-                var canget = $sibling.data("weightedWidth") - optns.minWidth;
-                if (canget >= lneed)
-                {
-                    canget = lneed;
-                    lneed  = 0;
-                } else {
-                    lneed -= canget;
-                }
-
-                $sibling.stop(true, false)
-                        .animate({
-                            left  : $sibling.data("weightedLeft") - lneed + "px",
-                            width : $sibling.data("weightedWidth") - canget + "px"
-                        });
-                $sibling = $sibling.prev();
-            }
-        }
-
-        function resizePathControl()
+        $(window).resize(function()
         {
             var w = $div.innerWidth();
 
@@ -183,39 +99,152 @@ define(function(require) {
                 if (optns.minWidth > itemWidth) { itemWidth = optns.minWidth; }
                 temp += itemWidth;
 
-                $child.data("weightedWidth", itemWidth).width(itemWidth);
+                $child.data("weightedWidth", itemWidth)
+                      .css({
+                        right : "auto",
+                        width : itemWidth + "px"
+                      });
             }
 
             // Arrange
-            arrangePathControl();
-        }
-
-        function arrangePathControl()
-        {
             var $children = $div.children(),
-                itemCount = $children.length,
-                i = l = left = 0;
+                l = left = 0;
 
-            for(; i<itemCount; ++i)
+            for(i = 0; i < itemCount; ++i)
             {
-                var $ch = $($children[i]);
+                var $ch          = $($children[i]),
+                    itemWidth    = $ch.outerWidth(),
+                    visibleWidth = $ch.data("weightedWidth"),
+                    padding      = Math.abs(itemWidth - $ch.width());
                 if (control.firstLowest)
                 {
                     l = left;
                 } else {
-                    l = left - Math.abs($ch.outerWidth() - $ch.width());
+                    l = left - padding;
                 }
 
-                left += $ch.css("left", l).data("weightedLeft", l).width();
+                left += $ch.css({
+                                left  : l,
+                                right : w - l - itemWidth,
+                                width : "auto"
+                            }).data({
+                                "weightedLeft" : l,
+                                "padding"      : padding
+                            }).width();
+            }
+        }).resize();
+
+        function aniPcItem($item, parentWidth, left, width)
+        {
+            $item.stop(true, false)
+                 .animate(
+                 {
+                    left  : left + 'px',
+                    right : (parentWidth - left - width - $item.data("padding")) + 'px'
+                 }, optns.duration);
+        }
+
+        function pcItemBlur()
+        {
+            // Animate to weighted width
+            var w = $div.width();
+            $div.children()
+                .each(function()
+                {
+                    var t = $(this);
+                    aniPcItem(t, w, t.data("weightedLeft"), t.data("weightedWidth"));
+                });
+        }
+
+        function pcItemHover() 
+        {
+            // Animate to prefered width
+            var $this = $(this),
+                pw    = $this.data("preferWidth"),
+                need  = pw - $this.data("weightedWidth");
+            if (need <= 0) { return; }
+
+            var $sibling  = $this.next(),
+                parentW   = $div.width(),
+                gain      = 0,
+                rGains    = [],
+                rSiblings = [];
+
+            while ($sibling.size() != 0 && gain < need) 
+            {
+                rSiblings.push($sibling);
+
+                var canget = $sibling.data("weightedWidth") - optns.minWidth;
+                if (canget + gain > need) { canget = need - gain; }
+                gain += canget;
+
+                rGains.push(canget);
+                $sibling = $sibling.next();
+            }
+
+            var lneed = need - gain;
+
+            if(gain != 0) 
+            {
+                var rgain = gain;
+                for(i = 0; i < rSiblings.length; ++i)
+                {
+                    $sibling = rSiblings[i]
+                    aniPcItem($sibling, 
+                              parentW, 
+                              $sibling.data("weightedLeft") + rgain,
+                              $sibling.data("weightedWidth") - rGains[i]);
+                    rgain -= rGains[i];
+                }
+            }
+
+            var prevSize = $this.prevAll().size();
+            if(lneed > 0 && prevSize > 0)
+            {
+                var leftMinWidth = prevSize * optns.minWidth,
+                    leftMaxGain  = $this.data("weightedLeft") - leftMinWidth;
+
+                if (!control.firstLowest) {
+                    leftMaxGain += $this.outerWidth() - $this.width();
+                }
+
+                if (leftMaxGain <= 0) {
+                    lneed = 0;
+                } else if(lneed > leftMaxGain) {
+                    lneed = leftMaxGain;
+                }
+            }
+
+            var tleft  = $this.data("weightedLeft") - lneed;
+            if (tleft > 0 && (gain > 0 || lneed > 0))
+            {
+                aniPcItem($this, parentW, tleft, pw);
+            }
+
+            $sibling = $this.prev();
+            while(lneed > 0 && $sibling.size() != 0) 
+            {
+                var canget = $sibling.data("weightedWidth") - optns.minWidth;
+                if (canget >= lneed)
+                {
+                    canget = lneed;
+                    lneed  = 0;
+                } else {
+                    lneed -= canget;
+                }
+
+                aniPcItem($sibling, 
+                          parentW, 
+                          $sibling.data("weightedLeft") - lneed, 
+                          $sibling.data("weightedWidth") - canget);
+                $sibling = $sibling.prev();
             }
         }
     }
 
 $(function(){
-
     PathControl("#proPC", skillDat.pro);
     PathControl("#basicPC", skillDat.basic);
-
 });
 
 });
--- js/skillDat.js
+++ js/skillDat.js
@@ -9,54 +9,37 @@ define({
                 title  : "Flash",
                 id     : "flashSk",
                 lv     : 4,
-                detail : "<div class='star'><span class='star4'></span></div>\
-<p>07年开始接触Flash，曾经为企业做过一个Flash编辑器。</p>\
-<ul><li class='tag'>\
-<span class='tagMain'>AS</span>\
-<span class='tagSub'>3</span></li></ul>"
+                detail : "<p>07年开始接触Flash，曾经为企业做过一个Flash编辑器。</p>",
+                tags   : ["AS", "3"]
             },
 
             {
                 title  : "C++",
                 id     : "cppSk",
                 lv     : 4,
-                detail : "<div class='star'><span class='star4'></span></div> \
-<p>经验值为1.5年。用过Qt做过桌面应用和用Win32 API做过一个界面库。没有接触过微软的MFC。</p> \
-<p>后台经验值为0，不适合编写数据密集的程序。</p> \
-<ul><li class='tag'><span class='tagMain'>Qt</span> \
-<span class='tagSub'>Ambassador</span></li> \
-<li class='tag'><span class='tagMain'>Win32</span> \
-<span class='tagSub clean'></span></li></ul>"
+                detail : "<p>经验值为1.5年。用过Qt做过桌面应用和用Win32 API做过一个界面库。没有接触过微软的MFC。</p><p>后台经验值为0，不适合编写数据密集的程序。</p>",
+                tags   : ["Qt", "Ambassador", "Win32", ""]
             },
 
             {
                 title  : "Web",
                 id     : "webSk",
                 lv     : 3,
-                detail : "<div class='star'><span class='star3'></span></div> \
-<p>请问哪里有好的Reference???</p> \
-<ul><li class='tag'> \
-<span class='tagMain'>jQuery</span> \
-<span class='tagSub clean'></span></li> \
-<li class='tag'>  \
-<span class='tagMain'>SCSS CSS3</span> \
-<span class='tagSub clean'></span></li> \
-<li class='tag'> \
-<span class='tagMain'>HTML5</span> \
-<span class='tagSub clean'></span></li></ul>"
+                detail : "<p>请问哪里有好的Reference???</p>",
+                tags   : ["jQuery", "", "CSS", "3", "SCSS", "", "HTML", "5"]
             },
 
             {
                 title  : "Obj-C",
                 id     : "objcSk",
                 lv     : 2,
-                detail : "<div class='star'><span class='star2'></span></div><p>Using obj-c is kinda fun. lol~</p>"
+                detail : "<p>Using obj-c is kinda fun. lol~</p>"
             },
 
             {
                 title  : "More",
                 id     : "moreSk",
-                lv     : 1,
+                lv     : 0,
                 detail : "<p>There'll be more whenever I need to.</p>"
             }
         ]
@@ -70,54 +53,38 @@ define({
                 title  : "English",
                 id     : "englishSk",
                 lv     : 5,
-                detail : "<div class='star'><span class='star5'></span></div> \
-<p>英语基本没有难度，交流，提问，文档都OK。口语有待加强。</p> \
-<ul><li class='tag'><span class='tagMain'>CET</span> \
-<span class='tagSub'>6</span></li></ul>"
+                detail : "<p>英语基本没有难度，交流，提问，文档都OK。口语有待加强。</p>",
+                tags   : ["CET", "6"]
             },
 
             {
                 title  : "关联学习",
                 id     : "studySk",
                 lv     : 3,
-                detail : "<div class='star'><span class='star3'></span></div><p>将会快速上手其他技术。</p>"
+                detail : "<p>将会快速上手其他技术。</p>"
             },
 
             {
                 title  : "前沿触觉",
                 id     : "senseSk",
                 lv     : 3,
-                detail : "<div class='star'><span class='star3'></span></div><p>比较关注崭新应用。对新型互联网服务也很感兴趣。</p>"
+                detail : "<p>比较关注崭新应用。对新型互联网服务也很感兴趣。</p>"
             },
 
             {
                 title  : "基本专业技能",
                 id     : "basicSk",
                 lv     : 5,
-                detail : "<div class='star'><span class='star5'></span></div> \
-<p>大学量产型软件专业学生，其实很多连基本电脑知识都不具备。相比之下，我折腾过很多系统。</p> \
-<ul><li class='tag'><span class='tagMain'>Windows</span> \
-<span class='tagSub clean'></span></li> \
-<li class='tag'><span class='tagMain'>Mac</span> \
-<span class='tagSub clean'></span></li> \
-<li class='tag'><span class='tagMain'>Android</span> \
-<span class='tagSub clean'></span></li> \
-<li class='tag'><span class='tagMain'>Ubuntu</span> \
-<span class='tagSub clean'></span></li>"
+                detail : "<p>大学量产型软件专业学生，其实很多连基本电脑知识都不具备。相比之下，我折腾过很多系统。</p>",
+                tags   : ["Ubuntu", "", "Mac", "", "Android", "", "Windows", ""]
             },
 
             {
                 title  : "问题解决能力",
                 id     : "solveSk",
                 lv     : 5,
-                detail : "<div class='star'><span class='star5'></span></div> \
-<p>有疑问就Search吧,骚年~</p> \
-<ul><li class='tag'><span class='tagMain'>墙</span> \
-<span class='tagSub'>不是问题</span></li> \
-<li class='tag'><span class='tagMain'>Google</span> \
-<span class='tagSub clean'></span></li> \
-<li class='tag'><span class='tagMain'>StackOverflow</span> \
-<span class='tagSub clean'></span></li></ul>"
+                detail : "<p>有疑问就Search吧,骚年~</p>",
+                tags   : ["Wall?", "Np", "Google", "", "StackOverflow", ""]
             }
         ]
     }
