9c3cf4cf6b6c246595cfe834592d02aed092c522
wwmBlog <lmorris@qq.com>
2012-02-19 20:53:26 +0800

    Nothing new
    Signed-off-by: wwmBlog <lmorris@qq.com>

--- app.build.js
+++ app.build.js
@@ -3,5 +3,5 @@
     optimize: "none",
     dir: "../build",
     optimizeCss: "none",
-    fileExclusionRegExp: /(^\.|psd|gae|scss|build|js)/
+    fileExclusionRegExp: /(^\.|psd|gae|scss|build|js|res)/
 })
