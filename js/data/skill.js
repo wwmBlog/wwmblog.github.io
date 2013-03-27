5452108aea6ed940d8a11a2d8a51f49ed7ca1308
WarWithinMe <liangmorr@gmail.com>
2013-03-27 21:04:04 +0800

    Restructure SVG related stuff, change data format

--- js/data/skill.js
+++ js/data/skill.js
@@ -1,16 +1,53 @@
 define({
-    pro   : [
-        { n : "AS3/Flash", l : 4 }
-      , { n : "AS3/Flash", l : 1 }
-      , { n : "AS3/Flash", l : 3 }
-      , { n : "AS3/Flash", l : 4 }
-      , { n : "AS3/Flash", l : 4 }
-      , { n : "AS3/Flash", l : 3 }
-    ]
-  , basic : [
-        { n : "AS3/Flash", l : 2 }
-      , { n : "AS3/Flash", l : 3 }
-      , { n : "AS3/Flash", l : 4 }
-      , { n : "AS3/Flash", l : 3, delay : true }
-    ]
-})
+
+    labels   : ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+  , datasets : [
+    {
+        fill          : "#fff"
+      , fillOpacity   : 0.3
+      , stroke        : "#000"
+      , strokeOpacity : 1
+      , strokeWidth   : 1
+      , data : [
+          null
+        , { l : 3, name : "ABC", extra : "" }
+
+        , null
+        , null
+
+        , { l : 3, name : "ABC", extra : "" }
+        , null
+
+        , { l : 3, name : "ABC", extra : "" }
+        , null
+
+        , null
+        , { l : 3, name : "ABC", extra : "" }
+      ]
+    }
+    , {
+        fill          : "#fff"
+      , fillOpacity   : 0.3
+      , stroke        : "#000"
+      , strokeOpacity : 1
+      , strokeWidth   : 1
+      , data : [
+          { l : 3, name : "ABC", extra : "" }
+        , null
+
+        , { l : 3, name : "ABC", extra : "" }
+        , { l : 3, name : "ABC", extra : "" }
+
+        , null
+        , { l : 3, name : "ABC", extra : "" }
+
+        , null
+        , { l : 3, name : "ABC", extra : "" }
+
+        , { l : 3, name : "ABC", extra : "" }
+        , null
+      ]
+    }
+  ]
+
+});
--- js/src/skillchart.js
+++ js/src/skillchart.js
@@ -1,52 +1,69 @@
 define(function(require){
 
   require("src/misc.js");
-  var SVG  = require("SVG");
-  var data = require("data/skill.js");
-  var draw = SVG("W_skillChart");
 
-  var data_pro   = data.pro;
-  var data_basic = data.basic;
-  var dotCount   = data_pro.length + data.basic.length;
-  var RADIUS     = 110;
-  var CENTER     = 230 / 2;
+  /* Graph */
+  function Graph( data, canvas ) {
+    this.data   = data;
+    this.canvas = canvas;
+    this.prototype.graphs.push(this);
+  }
+  Graph.prototype.draw = function () {
 
-  function cos( deg ) { return Math.cos( deg * Math.PI / 180 ); }
-  function sin( deg ) { return Math.sin( deg * Math.PI / 180 ); }
-
-  $("#W_skillChart").on("autoresize", rerender);
-
-  function rerender() {
-    draw.clear();
-    beforeRender();
-    renderBG();
   }
 
-  function beforeRender () {
-    var container = $("#W_skillChart");
-    CENTER = container.width() / 2;
-    RADIUS = CENTER - 5;
+
+  /* Canvas */
+  var SVG    = require("SVG");
+  var canvas = SVG("W_skillChart");
+  canvas.setDataSet = function ( dataset ) {
+    this.data = dataset;
+    this.render();
   }
 
-  function renderBG () {
-    var angle     = 360 / dotCount;
-    var colors    = ["#e2ddce", "#d8d1be", "#d1c8b1", "#cac1a9", "#c5bca3"];
-    var color_len = colors.length;
-    for ( var i = 0; i < color_len; ++i ) {
+  canvas.render = function () {
+    this.clear();
+    this.setup();
+    this.renderBG();
+  }
+
+  canvas.setup = function () {
+    var $c = $("#W_skillChart");
+
+    this.CENTER = $c.width() / 2;
+    this.RADIUS = this.CENTER - 5; 
+  }
+
+  canvas.renderBG = function () {
+    var labels = this.data.labels;
+    var angle  = 360 / labels.length;
+    var colors = ["#e2ddce", "#d8d1be", "#d1c8b1", "#cac1a9", "#c5bca3"];
+    var c_len  = colors.length;
+
+    for ( var i = 0; i < c_len; ++i ) {
       var string = [];
-      var r      = Math.floor((color_len - i) / color_len * RADIUS);
-      for ( var j = 0; j < dotCount; ++j )
+      var r      = Math.floor((c_len - i) / c_len * this.RADIUS);
+
+      for ( var j = 0; j < labels.length; ++j )
       {
         var a = 90 - angle * j;
         var x = Math.floor( cos(a) * r );
         var y = Math.floor( sin(a) * r );
         string.push(x + "," + y);
       }
-      draw.polygon(string.join(" "), true)
+      
+      this.polygon(string.join(" "), true)
           .fill(colors[i])
-          .move(CENTER, CENTER);
+          .move(this.CENTER, this.CENTER);
     }
-  };
+  }
 
-  rerender();
+  /* Helpers */
+  function cos( deg ) { return Math.cos( deg * Math.PI / 180 ); }
+  function sin( deg ) { return Math.sin( deg * Math.PI / 180 ); }
+
+  /* Data / Entrance */
+  var data = require("data/skill.js");
+  canvas.setDataSet( data );
+  $("#W_skillChart").on("autoresize", function(){ canvas.render(); });
 });
