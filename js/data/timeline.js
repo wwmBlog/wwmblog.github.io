f4a645924d30a294b67c4a073cc3b5f37aea186e
WarWithinMe <liangmorr@gmail.com>
2013-05-02 00:36:59 +0800

    updated icons

--- js/data/timeline.js
+++ js/data/timeline.js
@@ -1,17 +1,17 @@
 define({
     current : [
-        { y : "'03", left : "15%", desc : "获得了一台古董电脑" }
-      , { y : "'07", left : "35%", desc : "用Flash做了一个网站" }
-      , { y : "'08", left : "45%", desc : "赚了第一笔钱" }
-      , { y : "'08", left : "50%", desc : "上大学，orz" , alpha : true, beta : true }
-      , { y : "'10", left : "60%", desc : "弄了一个桌面APP" }
-      , { y : "'11", left : "75%", desc : "GUI库开发了一半" }
-      , { y : "'12", left : "85%", desc : "转型做前端" }
-      , { y : "'12", left : "90%", desc : "买了一台rMBP" }
+        { y : "'03", left : "15%", icon : "", desc : "获得了一台古董电脑" }
+      , { y : "'07", left : "35%", icon : "", desc : "用Flash做了一个网站" }
+      , { y : "'08", left : "45%", icon : "", desc : "赚了第一笔钱" }
+      , { y : "'08", left : "50%", icon : "", desc : "上大学，orz" , alpha : true, beta : true }
+      , { y : "'10", left : "60%", icon : "", desc : "开发桌面APP" }
+      , { y : "'11", left : "75%", icon : "", desc : "GUI库完成了一半" }
+      , { y : "'12", left : "85%", icon : "", desc : "转型做前端" }
+      , { y : "'12", left : "90%", icon : "", desc : "买了一台rMBP" }
     ]
-  , alpha : [ { y : "'12", left : "82%", desc : "制作激动人心的游戏" } ]
-  , beta  : [ { y : "'10", left : "65%", desc : "拍摄有灵魂的电影" } 
-            , { y : "'14", left : "95%", desc : "成为新晋导演" } ]
-  , start : { y : "'90", desc : "我出生了" }
-  , end   : { y : "'99", desc : "世界继续运行" }
+  , alpha : [ { y : "'12", left : "82%", icon : "", desc : "制作激动人心的游戏" } ]
+  , beta  : [ { y : "'10", left : "65%", icon : "", desc : "拍摄有灵魂的电影" } 
+            , { y : "'14", left : "95%", icon : "", desc : "成为新晋导演" } ]
+  , start : { y : "'90", icon : "", desc : "我出生了" }
+  , end   : { y : "'99", icon : "", desc : "世界继续运行" }
 });
